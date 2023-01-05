2caad5f2a686acee6c961f6040609be9b6a429c8
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-05 17:20:11 -0500

    middleware, parsing enabled, fixed links to js

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ const fs = require("fs");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
