fcbb18017f9c2a2fc7867e77c76dd50860ff1397
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-06 18:16:41 -0500

    added get mehtod for * path

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get("/api/notes", (req, res) => {
   res.json(JSON.parse(retrievedData));
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
